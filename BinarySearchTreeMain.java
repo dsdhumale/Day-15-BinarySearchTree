3b7d3f7ef13647fde8be5d1f21d34e177df8d9ca
dsdhumale <d.sdhumale@gmail.com>
2022-10-31 02:58:32 +0530

    Added : Welcome Message

---
+++ BinarySearchTreeMain.java
@@ -0,0 +1,7 @@
+package com.Bridgelabz.BinarySearchTree;
+
+public class BinarySearchTreeMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome in Binary Search Tree program");
+	}
+}
