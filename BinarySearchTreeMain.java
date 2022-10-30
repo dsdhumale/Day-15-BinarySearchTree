58f184aa5006d94fcd90454fa6adfd69b7081037
dsdhumale <d.sdhumale@gmail.com>
2022-10-31 03:06:43 +0530

    Added : Ability to add elements in binary search tree

--- BinarySearchTreeMain.java
+++ BinarySearchTreeMain.java
@@ -3,5 +3,16 @@ package com.Bridgelabz.BinarySearchTree;
 public class BinarySearchTreeMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome in Binary Search Tree program");
+		BinarySearchTree tree = new BinarySearchTree();
+		// Add node to binary tree
+		tree.insert(56);
+		tree.insert(30);
+		tree.insert(70);
+
+		System.out.println("\nBinary search tree after insertion of element");
+
+		// Display the binary tree
+		tree.inorderDisplay(tree.root);
 	}
+
 }
---
+++ Node.java
@@ -0,0 +1,11 @@
+package com.Bridgelabz.BinarySearchTree;
+
+public class Node {
+	int data;
+	Node left;
+	Node right;
+
+	Node(int data) {
+		this.data = data;
+	}
+}
